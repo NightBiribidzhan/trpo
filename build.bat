def994449960a5376b66dd6e9d6f1dd80504b739
429273-19 <429273-19>
2019-12-12 13:47:38 +0300

    build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 path=H:\Cremza\PortableGit\bin;%path%
 git symbolic-ref --short -q HEAD>version
-git clone https://github.com/rok9ru/trpo-core core
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
