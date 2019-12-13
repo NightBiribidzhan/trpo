220fa540d581183927a8415c8439bb5def445354
429273-19 <429273-19>
2019-12-13 14:17:56 +0300

    eblo off

--- indextest.php
+++
@@ -1,7 +0,0 @@
-<?php
-$url_to_file = 'https://getcomposer.org/installer';
-$my_save_dir = './';
-$filename = basename('composer-setup.php');
-$complete_save_loc = $my_save_dir . $filename;
-file_put_contents($complete_save_loc, file_get_contents($url_to_file));
-?>
