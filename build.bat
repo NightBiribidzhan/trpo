0086187705e7fb74df2ee346182e3d36128a2f11
429273-19 <429273-19>
2019-12-13 15:06:44 +0300

    build.bat

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ git symbolic-ref --short -q HEAD>version
 
 
 
-php -r "$url_to_file = 'https://getcomposer.org/installer'; $my_save_dir = 'trpo/'; $my_save_dir = 'trpo/';$filename = basename($url_to_file); $complete_save_loc = $my_save_dir . $ilename; file_put_contents($complete_save_loc, file_get_contents($url_to_file));"
+php -r "$url_to_file = 'https://getcomposer.org/installer'; $my_save_dir = '/'; $filename = basename($url_to_file); $complete_save_loc = $my_save_dir . $ilename; file_put_contents($complete_save_loc, file_get_contents($url_to_file));"
 
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
