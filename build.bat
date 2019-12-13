12603ddf71be6b691bc075651d82278256968c75
429273-19 <429273-19>
2019-12-13 15:39:12 +0300

    build.bat

--- build.bat
+++ build.bat
@@ -1,10 +1,8 @@
 path=H:\Cremza\PortableGit\bin;%path%
 git symbolic-ref --short -q HEAD>version
 
-
-
-php -r "$url_to_file = 'https://getcomposer.org/installer'; $my_save_dir = ''; $filename = basename("composer-setup.php"); $complete_save_loc = $my_save_dir . $filename; file_put_contents($complete_save_loc, file_get_contents($url_to_file));"
-
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'baf1608c33254d00611ac1705c1d9958c817a1a33bce370c0595974b342601bd80b92a3f46067da89e3b06bff421f182') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 PAUSE
