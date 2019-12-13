7adf2385e6d60981a13b43489812f38542ff8641
429273-19 <429273-19>
2019-12-13 16:04:26 +0300

    ez

--- index.php
+++ index.php
@@ -4,18 +4,26 @@ error_reporting(-1);
 
 require __DIR__ . '/vendor/autoload.php';
 
+include_once("vendor/rok9ru/trpo-core/EquationInterface.php");
+include_once("vendor/rok9ru/trpo-core/LogInterface.php");
+include_once("vendor/rok9ru/trpo-core/LogAbstract.php");
+include_once("vendor/nightbiribidzhann/zxc/BamboniException.php");
+include_once("vendor/nightbiribidzhann/zxc/Linear.php");
+include_once("vendor/nightbiribidzhann/zxc/Log.php");
+include_once("vendor/nightbiribidzhann/zxc/Quadratic.php");
+
 $co_arr = [];
+$version = fopen("version", "r");
+
 foreach (["a", "b", "c"] as $co) {
     echo "Enter " . $co . ": ";
     $line = stream_get_line(STDIN, 1024, PHP_EOL);
     $co_arr[$co] = $line === "" ? 0 : $line;
 }
-
-zxc\Log::log('Version '.file_get_contents('./version'));
-
 $a = $co_arr["a"];
 $b = $co_arr["b"];
 $c = $co_arr["c"];
+zxc\Log::log('Version '.file_get_contents('version'));
 //zxc\Log::log("Entered numbers: " . implode(", ", $co_arr));
 
 try {
