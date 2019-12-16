07fbea5d5beceab8139afa0a0ef4eb2e57351c12
429273-19 <429273-19>
2019-12-16 15:06:05 +0300

    ez

--- index.php
+++ index.php
@@ -15,20 +15,10 @@ include_once("vendor/nightbiribidzhann/zxc/Quadratic.php");
 $co_arr = [];
 $version = fopen("version", "r");
 
-foreach (["a", "b", "c"] as $co) {
-    echo "Enter " . $co . ": ";
-    $line = stream_get_line(STDIN, 1024, PHP_EOL);
-    $co_arr[$co] = $line === "" ? 0 : $line;
-}
 $a=readline("a=");
 $b=readline("b=");  
 $c=readline("c=");    
-//$a = $co_arr["a"];
-//$b = $co_arr["b"];
-//$c = $co_arr["c"];
 zxc\Log::log('Version '.file_get_contents('version'));
-//zxc\Log::log("Entered numbers: " . implode(", ", $co_arr));
-
 try {
     zxc\Log::log("Equation: $a*x^2 + $b*x + $c = 0");
     $solver = new zxc\Quadratic($a, $b, $c);
